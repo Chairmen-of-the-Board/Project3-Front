d7b3af23f92c41d86e171662a8f78a3a1de476e2
Conro101 <cbot.596@gmail.com>
2022-12-21 11:30:05 -0500

    updated prod url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  url: 'http://Bankingbackendnew-env.eba-aa4pazzc.us-east-1.elasticbeanstalk.com:8080/',
+  url: 'http://Bankingbackendnew-env.eba-aa4pazzc.us-east-1.elasticbeanstalk.com/',
   headers: {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': 'http://localhost:4200',
